a4ca8bfa25c96c0f94a4e98ba542b846b3313d79
Buddha <buddhalama88480@gmail.com>
2022-12-02 20:42:31 -0800

    changed

--- main.js
+++ main.js
@@ -3,32 +3,37 @@ let search_button = document.getElementById("search_btn");
 let form = document.querySelector("form");
 let infos = document.querySelector(".other-info");
 let temp = document.querySelector(".temp-info");
-let mainSection = document.querySelector(".main-section");
 
-async function weatherDatas(city) {
+
+async function defaultData() {                        //returning default api data without searching
+  let data = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=kathmandu&appid=${apiKey}&units=metric`
+  );
+  let response = await data.json();
+  return showingAllDatas(response);
+}
+defaultData();
+
+async function searchedWeatherDatas(city) {                    //returning searched weather datas
+  
   let data = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
   );
   let response = await data.json();
-  return tempDatas(response);
+  return showingAllDatas(response);
 }
 
-function tempDatas(data) {
-  //showing temp and city name
+function showingAllDatas(data) {                  //returning datas from html
+ 
   temp.innerHTML = `  <p><span>${Math.floor(data.main.temp)}°C</span></p>
 <p>${data.name}</p>`;
-  return otherDatas(data);
-}
-
-function otherDatas(data) {
-  //showing humidity, feelslike
-  console.log(data);
   infos.innerHTML = ` <p>Feels like: ${Math.floor(data.main.feels_like)}°C </p>
 <p>Humidity: ${data.main.humidity}% </p>
 <p>Sky: ${data.weather[0].description}</p>`;
 }
 
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", function (e) {          //returning api datas from searched value
   e.preventDefault();
-  weatherDatas(search_button.value);
+  searchedWeatherDatas(search_button.value);
 });
+
